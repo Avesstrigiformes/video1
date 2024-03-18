2f6d6e386c51490f5a4a6ae733bc4f946b96df72
novissima <112693247+Avesstrigiformes@users.noreply.github.com>
2024-03-18 23:03:45 +0300

    Update thumbnail.py

--- thumbnail.py
+++ thumbnail.py
@@ -1,5 +1,5 @@
 from manim import *
-class Proof(Scene):      
+class Thumbnail(Scene):      
     def construct(self):  
         t1 = MathTex(r"x^{3}+y^{3}=p(xy+p)", color= BLUE).scale(2)
         rect = SurroundingRectangle(t1, fill_color=WHITE, fill_opacity=0.15, buff=1, color=ORANGE)
